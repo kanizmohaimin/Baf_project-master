bef368c4c01383a5fd2b5cd9823bbf55431b653d
kaniz <kf16641567@gmail.com>
2018-09-30 19:37:55 +0600

    commit kaniz6

--- app/src/main/java/com/example/m/baf_project/MainActivity.java
+++ app/src/main/java/com/example/m/baf_project/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mstartbtn=(Button)findViewById(R.id.startbtn);
+
     }
 
     public void start(View view){
